b6f71e91ff4e05e04588545116ea7f498711898b
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-11-19 10:48:12 +0000

    futher improvements

--- BallycannonFarm/app/src/main/java/ie/ul/deirdreshanahan/ballycannonfarm/Constants.java
+++ BallycannonFarm/app/src/main/java/ie/ul/deirdreshanahan/ballycannonfarm/Constants.java
@@ -13,4 +13,5 @@ public class Constants {
 	public static final String KEY_SEX = "Sex";
     public static final String KEY_CREATED = "created";
     public static final String EXTRA_DOC_ID = "document_id";
+    public static final String EMPTY = "";
 }
